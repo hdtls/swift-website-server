c161d83de8af7aad4ccfe4040dc14d7efc448856
Junfeng Zhang <jeffzx15o@gmail.com>
2020-09-21 19:21:12 +0800

    Update project model.

--- Sources/App/Migrations/ProjMigration.swift
+++ Sources/App/Migrations/ProjMigration.swift
@@ -7,22 +7,35 @@ extension Project {
     class Migration: Fluent.Migration {
 
         func prepare(on database: Database) -> EventLoopFuture<Void> {
-            var enumBuilder = database.enum(Kind.schema)
-            Kind.allCases.forEach({
-                enumBuilder = enumBuilder.case($0.rawValue)
+            var kindBuilder = database.enum(ProjKind.schema)
+            ProjKind.allCases.forEach({
+                kindBuilder = kindBuilder.case($0.rawValue)
             })
 
-            return enumBuilder.create()
+            var visibilityBuilder = database.enum(ProjVisibility.schema)
+            ProjVisibility.allCases.forEach({
+                visibilityBuilder = visibilityBuilder.case($0.rawValue)
+            })
+
+            return kindBuilder.create()
+                .and(visibilityBuilder.create())
                 .flatMap({
                     database.schema(Project.schema)
                         .id()
                         .field(FieldKeys.user.rawValue, .uuid, .references(User.schema, .id))
                         .field(FieldKeys.name.rawValue, .string, .required)
+                        .field(FieldKeys.note.rawValue, .string)
                         .field(FieldKeys.genres.rawValue, .array(of: .string))
-                        .field(FieldKeys.summary.rawValue, .string, .required)
+                        .field(FieldKeys.summary.rawValue, .sql(raw: "VARCHAR(1024)"), .required)
                         .field(FieldKeys.artworkUrl.rawValue, .string)
+                        .field(FieldKeys.backgroundImageUrl.rawValue, .string)
+                        .field(FieldKeys.promoImageUrl.rawValue, .string)
                         .field(FieldKeys.screenshotUrls.rawValue, .array(of: .string))
-                        .field(FieldKeys.kind.rawValue, $0, .required)
+                        .field(FieldKeys.padScreenshotUrls.rawValue, .array(of: .string))
+                        .field(FieldKeys.kind.rawValue, $0.0, .required)
+                        .field(FieldKeys.visibility.rawValue, $0.1, .required)
+                        .field(FieldKeys.trackViewUrl.rawValue, .string)
+                        .field(FieldKeys.trackId.rawValue, .string)
                         .field(FieldKeys.startDate.rawValue, .string, .required)
                         .field(FieldKeys.endDate.rawValue, .string, .required)
                         .create()
@@ -33,7 +46,10 @@ extension Project {
             database.schema(Project.schema)
                 .delete()
                 .flatMap({
-                    database.enum(Kind.schema).delete()
+                    database.enum(ProjKind.schema).delete()
+                })
+                .flatMap({
+                    database.enum(ProjVisibility.schema).delete()
                 })
         }
     }
--- Sources/App/Models/Proj.swift
+++ Sources/App/Models/Proj.swift
@@ -1,7 +1,41 @@
 import Vapor
 import Fluent
 
-final class Project: Model {
+enum ProjKind: String, CaseIterable, Codable {
+    static let schema: String = "project_kinds"
+
+    case app
+    case website
+    case repositry
+}
+
+enum ProjVisibility: String, CaseIterable, Codable {
+    static let schema: String = "project_visibility"
+
+    case `private`
+    case `public`
+}
+
+protocol ProjProtocol {
+    var name: String { get set }
+    var note: String? { get set }
+    var genres: [String]? { get set }
+    var summary: String { get set }
+    var artworkUrl: String? { get set }
+    var backgroundImageUrl: String? { get set }
+    var promoImageUrl: String? { get set }
+    var screenshotUrls: [String]? { get set }
+    var padScreenshotUrls: [String]? { get set }
+    var kind: ProjKind { get set }
+    var visibility: ProjVisibility { get set }
+    var trackViewUrl: String? { get set }
+    var trackId: String? { get set }
+    var startDate: String { get set }
+    var endDate: String { get set }
+}
+
+final class Project: ProjProtocol, Model {
+
     typealias IDValue = UUID
 
     static var schema: String = "projects"
@@ -13,6 +47,9 @@ final class Project: Model {
     @Field(key: FieldKeys.name.rawValue)
     var name: String
 
+    @OptionalField(key: FieldKeys.note.rawValue)
+    var note: String?
+
     @OptionalField(key: FieldKeys.genres.rawValue)
     var genres: [String]?
 
@@ -22,11 +59,29 @@ final class Project: Model {
     @OptionalField(key: FieldKeys.artworkUrl.rawValue)
     var artworkUrl: String?
 
+    @OptionalField(key: FieldKeys.backgroundImageUrl.rawValue)
+    var backgroundImageUrl: String?
+
+    @OptionalField(key: FieldKeys.promoImageUrl.rawValue)
+    var promoImageUrl: String?
+
     @OptionalField(key: FieldKeys.screenshotUrls.rawValue)
     var screenshotUrls: [String]?
 
+    @OptionalField(key: FieldKeys.padScreenshotUrls.rawValue)
+    var padScreenshotUrls: [String]?
+
     @Field(key: FieldKeys.kind.rawValue)
-    var kind: Kind
+    var kind: ProjKind
+
+    @Field(key: FieldKeys.visibility.rawValue)
+    var visibility: ProjVisibility
+
+    @OptionalField(key: FieldKeys.trackViewUrl.rawValue)
+    var trackViewUrl: String?
+
+    @OptionalField(key: FieldKeys.trackId.rawValue)
+    var trackId: String?
 
     @Field(key: FieldKeys.startDate.rawValue)
     var startDate: String
@@ -46,22 +101,21 @@ extension Project {
 
     enum FieldKeys: FieldKey {
         case name
+        case note
         case genres
         case summary
         case artworkUrl = "artwork_url"
+        case backgroundImageUrl = "background_image_url"
+        case promoImageUrl = "promo_image_url"
         case screenshotUrls = "screenshot_urls"
+        case padScreenshotUrls = "pad_screenshot_urls"
         case kind
+        case visibility
+        case trackViewUrl = "track_view_url"
+        case trackId = "track_id"
         case startDate = "start_date"
         case endDate = "end_date"
         case user = "user_id"
-    }
-}
-
-extension Project {
-    enum Kind: String, CaseIterable, Codable {
-        static let schema: String = "proj_kind"
-
-        case software
     }
 }
 
@@ -69,14 +123,21 @@ extension Project: Serializing {
 
     typealias SerializedObject = Coding
 
-    struct Coding: Content, Equatable {
+    struct Coding: ProjProtocol, Content, Equatable {
         var id: IDValue?
         var name: String
+        var note: String?
         var genres: [String]?
         var summary: String
         var artworkUrl: String?
+        var backgroundImageUrl: String?
+        var promoImageUrl: String?
         var screenshotUrls: [String]?
-        var kind: Kind
+        var padScreenshotUrls: [String]?
+        var kind: ProjKind
+        var visibility: ProjVisibility
+        var trackViewUrl: String?
+        var trackId: String?
         var startDate: String
         var endDate: String
 
@@ -86,13 +147,14 @@ extension Project: Serializing {
 
     convenience init(content: SerializedObject) throws {
         self.init()
-        id = content.id
         name = content.name
+        note = content.note
         genres = content.genres
         summary = content.summary
-        artworkUrl = content.artworkUrl?.path
-        screenshotUrls = content.screenshotUrls?.compactMap({ $0.path })
         kind = content.kind
+        visibility = content.visibility
+        trackViewUrl = content.trackViewUrl
+        trackId = content.trackId
         startDate = content.startDate
         endDate = content.endDate
     }
@@ -101,11 +163,18 @@ extension Project: Serializing {
         try SerializedObject.init(
             id: requireID(),
             name: name,
+            note: note,
             genres: genres,
             summary: summary,
-            artworkUrl: artworkUrl?.absoluteURLString,
-            screenshotUrls: screenshotUrls?.map({ $0.absoluteURLString }),
+            artworkUrl: artworkUrl,
+            backgroundImageUrl: backgroundImageUrl,
+            promoImageUrl: promoImageUrl,
+            screenshotUrls: screenshotUrls,
+            padScreenshotUrls: padScreenshotUrls,
             kind: kind,
+            visibility: visibility,
+            trackViewUrl: trackViewUrl,
+            trackId: trackId,
             startDate: startDate,
             endDate: endDate,
             userId: $user.id
@@ -117,11 +186,18 @@ extension Project: Mergeable {
 
     func merge(_ other: Project) {
         name = other.name
+        note = other.note
         genres = other.genres
         summary = other.summary
-        artworkUrl = other.artworkUrl
+        artworkUrl = other.artworkUrl?.path
+        backgroundImageUrl = other.backgroundImageUrl?.path
+        promoImageUrl = other.promoImageUrl
         screenshotUrls = other.screenshotUrls
+        padScreenshotUrls = other.padScreenshotUrls
         kind = other.kind
+        visibility = other.visibility
+        trackViewUrl = other.trackViewUrl
+        trackId = other.trackId
         startDate = other.startDate
         endDate = other.endDate
     }
