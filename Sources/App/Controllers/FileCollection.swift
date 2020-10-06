3aeb844f7429f3192c1dd1e301ed4ef9e1773b33
Junfeng Zhang <jeffzx15o@gmail.com>
2020-10-06 19:14:29 +0800

    Replace file read with FileMiddleware.

--- Sources/App/Controllers/FileCollection.swift
+++ Sources/App/Controllers/FileCollection.swift
@@ -18,8 +18,6 @@ class FileCollection: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let routes = routes.grouped(.init(stringLiteral: type.rawValue))
 
-        routes.on(.GET, .anything, use: read)
-
         let trusted = routes.grouped([
             User.authenticator(),
             Token.authenticator(),
@@ -29,33 +27,8 @@ class FileCollection: RouteCollection {
         trusted.on(.POST, body: .collect(maxSize: maximumBodySize), use: create)
     }
 
-    func read(_ req: Request) throws -> EventLoopFuture<Response> {
-        // make a copy of the path
-        var path = req.url.path
-
-        // path must be relative.
-        while path.hasPrefix("/") {
-            path = String(path.dropFirst())
-        }
-
-        // protect against relative paths
-        guard !path.contains("../") else {
-            return req.eventLoop.makeFailedFuture(Abort(.forbidden))
-        }
-
-        // create absolute file path
-        let filePath = req.application.directory.publicDirectory + (path.removingPercentEncoding ?? path)
-
-        // check if file exists and is not a directory
-        var isDir: ObjCBool = false
-        guard FileManager.default.fileExists(atPath: filePath, isDirectory: &isDir), !isDir.boolValue else {
-            throw Abort(.notFound)
-        }
-
-        return req.eventLoop.makeSucceededFuture(req.fileio.streamFile(at: filePath))
-    }
-
     func create(_ req: Request) throws -> EventLoopFuture<MultipartFileCoding> {
+        req.content.contentType
         switch type {
         case .images:
             return try uploadImageFile(req)
