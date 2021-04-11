a0ff7ff4992dd6a2bc45446d807ea35f7aa31c39
Junfeng Zhang <jeffzx15o@gmail.com>
2021-04-11 14:34:40 +0800

    Remove App module.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "App", targets: ["App"]),
         .executable(name: "Run", targets: ["Run"]),
     ],
     dependencies: [
