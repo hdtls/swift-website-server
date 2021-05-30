268b236da05a73cc9436e095087c110b9c07c9fc
Junfeng Zhang <jeffzx15o@gmail.com>
2021-05-31 02:23:25 +0800

    Update user authentication.

--- Sources/App/Controllers/LogCollection.swift
+++ Sources/App/Controllers/LogCollection.swift
@@ -4,7 +4,11 @@ class LogCollection: RouteCollection {
 
     func boot(routes: RoutesBuilder) throws {
         routes.grouped(User.authenticator()).on(.POST, "login", use: logIn(_:))
-        routes.grouped(Token.authenticator()).on(.POST, "logout", use: logOut(_:))
+        routes.grouped(
+            Token.authenticator(),
+            User.authenticator(),
+            User.guardMiddleware()
+        ).on(.POST, "logout", use: logOut(_:))
     }
 
     func logIn(_ req: Request) throws -> EventLoopFuture<AuthorizeMsg> {
@@ -12,6 +16,9 @@ class LogCollection: RouteCollection {
         let token = try Token.init(user)
 
         return token.save(on: req.db)
+            .map({
+                req.auth.login(token)
+            })
         .flatMapThrowing({
             try AuthorizeMsg.init(user: user.dataTransferObject(), token: token)
         })
@@ -20,6 +27,10 @@ class LogCollection: RouteCollection {
     func logOut(_ req: Request) throws -> EventLoopFuture<HTTPResponseStatus> {
         try req.auth.require(Token.self)
             .delete(on: req.db)
+            .map({
+                req.auth.logout(Token.self)
+                req.auth.logout(User.self)
+            })
             .map({ .ok })
     }
 }
