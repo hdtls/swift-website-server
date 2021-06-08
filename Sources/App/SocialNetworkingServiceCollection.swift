78af5687ec2407b8b55d44c2ef9926d291c35beb
Junfeng Zhang <jeffzx15o@gmail.com>
2021-06-08 20:45:31 +0800

    Update social networking service types.

--- Sources/App/SocialNetworkingServiceCollection.swift
+++ Sources/App/SocialNetworkingServiceCollection.swift
@@ -2,13 +2,13 @@ import Vapor
 
 /// In progress
 /// admin user request.
-class SocialNetworkingServiceCollection: RestfulApiCollection {
+class SocialNetworkingServiceCollection: ApiCollection {
     typealias T = SocialNetworkingService
 
     var path: String = SocialNetworking.schema + "/services"
 
-    func performUpdate(_ original: T?, on req: Request) throws -> EventLoopFuture<T.Coding> {
-        let coding = try req.content.decode(T.SerializedObject.self)
+    func performUpdate(_ original: T?, on req: Request) throws -> EventLoopFuture<T.DTO> {
+        let coding = try req.content.decode(T.DTO.self)
         guard coding.type != nil else {
             throw Abort.init(.badRequest, reason: "Value required for key 'type'")
         }
