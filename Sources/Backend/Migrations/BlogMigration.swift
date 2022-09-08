b77fd00f10c0bb8b1dde4be9028af01e389cbfd2
Junfeng Zhang <jeffzx15o@gmail.com>
2022-09-08 10:33:52 +0800

    Increment blog excerpt size

--- Sources/Backend/Migrations/BlogMigration.swift
+++ Sources/Backend/Migrations/BlogMigration.swift
@@ -13,7 +13,7 @@ extension Blog {
                 .unique(on: FieldKeys.alias)
                 .field(FieldKeys.title, .string, .required)
                 .field(FieldKeys.artworkUrl, .string)
-                .field(FieldKeys.excerpt, .sql(raw: "VARCHAR(512)"))
+                .field(FieldKeys.excerpt, .sql(raw: "VARCHAR(1024)"))
                 .field(FieldKeys.tags, .array(of: .string))
                 .field(FieldKeys.content, .string, .required)
                 .field(FieldKeys.user, .int, .references(User.schema, .id))
